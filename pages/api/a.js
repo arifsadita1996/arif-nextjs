fdd103b53ae4061261d025b75dcd5c36c638eeae
lalalla <>
2023-08-21 03:42:40 -0700

    first commit

--- pages/api/a.js
+++ pages/api/a.js
@@ -1,3 +1,26 @@
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+
+  import sqlite3 from "sqlite3";
+import { open, Database } from "sqlite";
+
+// Let's initialize it as null initially, and we will assign the actual database instance later.
+let db = null;
+
+// Define the GET request handler function
+export default async function handler(req, res) {
+  // Check if the database instance has been initialized
+  if (!db) {
+    // If the database instance is not initialized, open the database connection
+    db = await open({
+      filename: "./mydb.db", // Specify the database file path
+      driver: sqlite3.Database, // Specify the database driver (sqlite3 in this case)
+    });
+  }
+
+
+  // Perform a database query to retrieve all items from the "items" table
+  const items = await db.all("SELECT * FROM buku");
+  console.log(items)
+  // Return the items as a JSON response with status 200
+ res.send(items)
 }
